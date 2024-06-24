39b46ff5e7210c289be21a50242050249c7abadd
Evan Guyot <92523408+evan-guyot@users.noreply.github.com>
2024-06-24 07:47:29 +0000

    Fix - Component Border Colors

--- app/components/Maker/ComponentList.tsx
+++ app/components/Maker/ComponentList.tsx
@@ -4,17 +4,19 @@ import { IComponent } from "@/app/lib/interfaces/maker";
 
 const ComponentList = ({ componentList }: { componentList: IComponent[] }) => {
   return (
-    <>
+    <div className="">
       {componentList.map((component, index) => {
         const Tag = component.type.tag as keyof JSX.IntrinsicElements;
 
         return (
           <Tag key={index} className={styles[Tag]}>
-            <div className="whitespace-pre-wrap">{component.value}</div>
+            <div className="whitespace-pre-wrap text-black dark:text-white decoration-black dark:decoration-white border-black dark:border-white">
+              {component.value}
+            </div>
           </Tag>
         );
       })}
-    </>
+    </div>
   );
 };
 
